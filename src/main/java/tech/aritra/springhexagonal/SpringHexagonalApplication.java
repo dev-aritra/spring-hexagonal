d7e62c67861f561be040573a9d91d21c2cbf45b6
dev-aritra <dev.aritradas@gmail.com>
2020-11-15 21:11:08 +0530

    Initial checkin

---
+++ src/main/java/tech/aritra/springhexagonal/SpringHexagonalApplication.java
@@ -0,0 +1,13 @@
+package tech.aritra.springhexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/aritra/springhexagonal/SpringHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.aritra.springhexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
